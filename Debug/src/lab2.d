9f8a8c252a4543d107820092a6cdec58cc35bb64
JacquelineE <jaceri@kth.se>
2016-08-11 05:41:26 +0200

    had to add files to pull

--- Debug/src/lab2.d
+++ Debug/src/lab2.d
@@ -1,3 +1,10 @@
-src/lab2.d: ../src/lab2.cpp ../src/date.hpp
+src/lab2.d: ../src/lab2.cpp ../src/date.hpp ../src/gregorian.hpp \
+ ../src/julian.hpp ../src/kattistime.hpp
 
 ../src/date.hpp:
+
+../src/gregorian.hpp:
+
+../src/julian.hpp:
+
+../src/kattistime.hpp:
